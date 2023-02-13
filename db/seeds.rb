195a2cf7a6db8e05e6aa73cec66df85072e0f0cb
wankoon puah puahwankoon@gmail.com <puahwankoon@gmail.com>
2023-02-13 21:54:37 +0800

    half way done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "faker"
+
+Restaurant.destroy_all
+
+puts "Clean all the restaurants"
+
+puts "Start create restaurants"
+
+10.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+  restaurant.save!
+end
+
+puts "Done"
